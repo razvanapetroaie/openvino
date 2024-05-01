c17bb1c33c4c4c32df3f9fc0db0a72e0d90373ad
Razvan Apetroaie <razvan-mihai.apetroaie@intel.com>
2024-05-01 13:05:41 +0000

    Using indices for serializing the I/O metadata when the compiler version is new enough

--- src/plugins/intel_npu/src/compiler/src/graph_transformations.cpp
+++ src/plugins/intel_npu/src/compiler/src/graph_transformations.cpp
@@ -34,7 +34,12 @@ IR serializeToIR(const std::shared_ptr<const ov::Model>& origModel, uint32_t sup
     // precision/layout preprocessing requirement. We are setting this value to "true" since the API version is no
     // longer a cause for altering the metadata. This is due to the preprocessing performed in the OpenVINO framework's
     // implementaion, the "ov::Model" object is preprocessed before reaching the NPU plugin.
-    const auto new_api_key = "is_new_api";
+    const auto newAPIKey = "is_new_api";
+
+    // Flag used for indicating an NPU plugin version which switched the identification convention of inputs/outputs
+    // from names to indices. The flag is required in order to inform the driver-compiler adapter to expect indices when
+    // attempting to deserialize the I/O metadata.
+    const auto useIndicesForIOMetadata = "use_indices_for_io_metadata";
 
     // We modify the original model object here therefore a mutex is required
     static std::mutex rtInfoMutex;
@@ -42,12 +47,14 @@ IR serializeToIR(const std::shared_ptr<const ov::Model>& origModel, uint32_t sup
     {
         std::lock_guard<std::mutex> lock(rtInfoMutex);
 
-        model->set_rt_info(true, new_api_key);
+        model->set_rt_info(true, newAPIKey);
+        model->set_rt_info(true, useIndicesForIOMetadata);
 
         manager.run_passes(model);
 
         auto& rtInfo = model->get_rt_info();
-        rtInfo.erase(new_api_key);
+        rtInfo.erase(newAPIKey);
+        rtInfo.erase(useIndicesForIOMetadata);
     }
 
     return {std::move(xmlStream), std::move(weightsStream)};
