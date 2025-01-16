dbcfe1dc3855a7838d0ac39579c1dcaaa5a68757
Nikita Kudriavtsev <nikita.kudriavtsev@intel.com>
2025-01-16 23:43:23 +0000

    Forcing the CiP path even if the compiler type was set to "DRIVER"

--- src/plugins/intel_npu/src/compiler_adapter/include/compiler_adapter_factory.hpp
+++ src/plugins/intel_npu/src/compiler_adapter/include/compiler_adapter_factory.hpp
@@ -18,18 +18,12 @@ public:
                                                         const Config& config) const {
         auto compilerType = config.get<COMPILER_TYPE>();
         switch (compilerType) {
+        case ov::intel_npu::CompilerType::DRIVER:
         case ov::intel_npu::CompilerType::MLIR: {
             if (engineBackend == nullptr || engineBackend->getName() != "LEVEL0") {
                 return std::make_unique<PluginCompilerAdapter>(nullptr);
             }
             return std::make_unique<PluginCompilerAdapter>(engineBackend->getInitStructs());
-        }
-        case ov::intel_npu::CompilerType::DRIVER: {
-            if (engineBackend == nullptr || engineBackend->getName() != "LEVEL0") {
-                OPENVINO_THROW("NPU Compiler Adapter must be used with LEVEL0 backend");
-            }
-
-            return std::make_unique<DriverCompilerAdapter>(engineBackend->getInitStructs());
         }
         default:
             OPENVINO_THROW("Invalid NPU_COMPILER_TYPE");
